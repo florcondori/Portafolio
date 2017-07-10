a76347ef8e5762ac725e3885c280389d6ccee42e
flor condori <condorihuamanif@gmail.com>
2017-07-10 13:20:12 -0500

    portafolio mediante js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,49 @@
 'use strict';
 $(_=>{
-	$(".proyecto").on("click", (e)=>{
-		console.log(e);
-		$(e.currentTarget).addClass("active");
-		$(".proyecto-social").slideDown(500);
-	});
 
 	$(".close").on("click", (e)=>{
 		$(".proyecto-social").slideUp(500);
-	});;
+	});
+
+	const container = $("#portafolio").children().eq(1).children();
+	console.log(proyectos);
+	proyectos.forEach( (obj)=>{
+		const ancla = $(`<a class='col-xs-12 col-sm-6 col-md-4 proyecto' id='${obj.id}'></a>`);
+		const img = $(`<img src='assets/img/${obj.img}' alt='${obj.title}'>`);
+		const bgProyecto = $("<div class='bg-proyecto'></div>");
+		const title = $(`<p>${obj.title}</p>`);
+		bgProyecto.hide();
+
+		ancla.on("click", (e)=>{
+			$(".proyecto-social").slideDown(600);
+			bgProyecto.show(300);
+			ancla.addClass("active");
+			console.log(ancla.prop("id"));
+			mostratDetalles(ancla.prop("id"));
+		});
+
+
+		bgProyecto.append(title);
+		ancla.append(img);
+		ancla.append(bgProyecto);
+
+		container.append(ancla);
+	});
 });
+
+const mostratDetalles = (id)=>{
+	const proyecto = proyectos.filter((obj)=>{
+		return obj.id == id;
+	})[0];
+	const reque = proyecto.requerimiento.join("</br>");
+	const use = proyecto.use.join(",");
+	const img = $(".proyecto-social").children().eq(1).children();
+	const divDescription = $(".proyecto-social").children().eq(2);
+
+	img.prop("src","assets/img/"+proyecto.img);
+	divDescription.append(`<h3>${proyecto.title}</h3>`);
+	divDescription.append(`<p>${proyecto.description}</p>`);
+	divDescription.append(`<p>${reque}</p>`);
+	divDescription.append(`<p>${use}</p>`);
+	divDescription.append(`<a href='${proyecto.url}' target="_blank" class="go-site">Sitio Web</a>`);
+}
---
+++ assets/js/utils/proyectos.js
@@ -0,0 +1,26 @@
+var proyectos = [
+	{id: 0,
+	 url: "https://florcondori.github.io/ProyectoFinal-LabCar/",
+	 img: "labcar.jpg",
+	 title: "LabCar",
+	 description:"Es una empresa de taxis.",
+	 requerimiento: ["Econtrar ubicacion de usuario", "Trazado de ruta", "Mostrar precio de viaje"],
+	 use: ["Bootstrap", "API de Google Maps"]
+	},
+	{id: 1,
+	 url: "https://florcondori.github.io/Freelancer/",
+	 img: "frelancer.jpg",
+	 title: "FreeLancer",
+	 description: "Template basado en Bootstrap",
+	 requerimiento: ["Hacer la barra de navegacion Fixed y efecto scroll","Portada de presentacion","La sección del portafolio usando grid y con hover", "La seccion de about, contact y footer"],
+	 use:["Flexbox"]
+	},
+	{id: 2,
+	 url: "https://florcondori.github.io/Gas-Finder/",
+	 img: "gasfinder.jpg",
+	 title: "Gas Finder",
+	 description: "Aplicacion que permite ubicar estaciones de servicio según el distrito",
+	 requerimiento: ["1°Pantalla: Buscar la estación de servicio por distrito", "2°Pantalla: Detalle de estación. Mostrar mapa con su ruta, así como la distancia"],
+	 use: ["API Gmaps", "Jquery"]
+	}
+];
