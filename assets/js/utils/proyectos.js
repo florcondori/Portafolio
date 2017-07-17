028f26838fff5e06bb8011d9f999d3501a229aea
flor condori <condorihuamanif@gmail.com>
2017-07-17 12:20:53 -0500

    2 proyectos mas

--- assets/js/utils/proyectos.js
+++ assets/js/utils/proyectos.js
@@ -1,26 +1,42 @@
 var proyectos = [
 	{id: 0,
-	 url: "https://florcondori.github.io/ProyectoFinal-LabCar/",
-	 git: "https://github.com/florcondori/ProyectoFinal-LabCar",
-	 img: "labcar.png",
-	 title: "LabCar",
-	 description:"Pagina web para una empresa de taxis. Permite ubicación y trazado de ruta.",
-	 use: ["Bootstrap", "API de Google Maps"]
+	 url: "https://sprint5-proyectofinal.herokuapp.com/",
+	 git: "https://github.com/florcondori/ProyectoFinal-Yape",
+	 img: "yape.png",
+	 title: "Yape",
+	 description:"Permite registro de usuario, validación de campos abligatorios, generar automaticamente código de validación(cada 21 seg.)",
+	 use: ["Jquery","API","CSS","HTML"]
 	},
 	{id: 1,
-	 url: "https://florcondori.github.io/Freelancer/",
-	 git:"https://github.com/florcondori/Freelancer",
-	 img: "frelancer.jpg",
-	 title: "FreeLancer",
-	 description: "Template basado en Bootstrap. Efc",
-	 use:["Flexbox", "Grid System"]
-	},
-	{id: 2,
 	 url: "https://florcondori.github.io/Gas-Finder/",
 	 git:"https://github.com/florcondori/Gas-Finder",
 	 img: "gasfinder.png",
 	 title: "Gas Finder",
-	 description: "Aplicacion que permite ubicar estaciones de servicio según el distrito",
-	 use: ["API Gmaps", "Jquery"]
+	 description: "Permite buscar estaciones de servicio por distrito, detalle de estación seleccionada y trazado de ruta.",
+	 use: ["API Gmaps", "Jquery", "HTML", "CSS"]
+	},
+	{id: 2,
+	 url: "https://florcondori.github.io/ProyectoFinal-LabCar/",
+	 git: "https://github.com/florcondori/ProyectoFinal-LabCar",
+	 img: "labcar.png",
+	 title: "LabCar",
+	 description:"Pagina web para una empresa de taxis. Encuentra tu ubicación, trazado de ruta y tarifa de viaje.",
+	 use: ["Bootstrap", "API de Google Maps"]
+	},
+	{id: 3,
+	 url: "https://florcondori.github.io/ProyectoFinal-Lyft/",
+	 git:"https://github.com/florcondori/ProyectoFinal-Lyft",
+	 img: "lyft.png",
+	 title: "Lyft",
+	 description: "Efecto de scroll, validación de formulario",
+	 use:["CSS", "HTML","JavaScript"]
+	},
+	{id: 4,
+	 url: "https://florcondori.github.io/Freelancer/",
+	 git:"https://github.com/florcondori/Freelancer",
+	 img: "frelancer.jpg",
+	 title: "FreeLancer",
+	 description: "Replica de un template basado en Bootstrap. Efecto scroll, validación de formulario, funcionalidad de modal",
+	 use:["Flexbox", "Grid System", "JavaScript"]
 	}
 ];
